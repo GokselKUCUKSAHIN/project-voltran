efc3503ed8492f4be1a8db2665d252d9d0362e28
jellybeanci <gokselkucuksahin@gmail.com>
2021-08-09 12:15:56 +0300

    getPrivateIP and getGatewayIP added.

--- cte/get-gateway-ip/ts/get-gateway-ip.ts
+++
@@ -1,4 +0,0 @@
-import * as network from "network";
-
-network.get_gateway_ip((err, ip) => console.log(err || ip));
-network.get_private_ip((err, ip) => console.log(err || ip));
---
+++ cte/get-gateway-ip/ts/get-ip.ts
@@ -0,0 +1,28 @@
+import * as network from "network";
+
+async function getCallback(callback: Function): Promise<string> {
+    return new Promise((res, rej) => {
+        callback((err, ip) => {
+            if (err) {
+                rej(`Error: ${err}`);
+            }
+            res(ip);
+        });
+    });
+}
+
+export async function getGatewayIP(): Promise<string> {
+    return getCallback(network.get_gateway_ip);
+}
+
+export async function getPrivateIP(): Promise<string> {
+    return getCallback(network.get_private_ip);
+}
+
+(async _ => {
+    try {
+        console.log(await getGatewayIP());
+    } catch (ex) {
+        console.log(ex);
+    }
+})();
