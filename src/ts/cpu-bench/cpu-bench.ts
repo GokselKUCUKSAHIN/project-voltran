d1b249ee827d1e26fa60f969d61381f685c59a86
Jellybeanci <gokselkucuksahin@gmail.com>
2021-08-15 22:32:49 +0300

    CPU Benchmark added.

---
+++ src/ts/cpu-bench/cpu-bench.ts
@@ -0,0 +1,15 @@
+export function CPUBench(count: number = 1): number {
+  const startTime = Date.now();
+  for (let i = 0; i < count; i++) {
+    coreLoad();
+  }
+  const deltaTime = Date.now() - startTime;
+  return 10_000_000 / deltaTime;
+}
+
+function coreLoad(load: number = 99_000_000): void {
+  let pi = 1;
+  for (let i = 2; i < load; i += 2) {
+    pi *= (i / (i - 1)) * (i / (i + 1));
+  }
+}
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,6 +1,9 @@
+import {CPUBench} from "./cpu-bench/cpu-bench";
+
 export class Test {
   static main(): void {
     console.log("Hello, World!");
+    console.log(CPUBench(5));
   }
 }
 
